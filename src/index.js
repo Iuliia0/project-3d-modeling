08b991c39412c3f8263714c855bc92e3519cf168
Iuliia Emelianova <julia03951@gmail.com>
2021-12-28 13:11:38 +0300

    Добавлена валидация для урока21

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
+import validation from './modules/validation'
 
 
 timer('1 january 2022')
 menu()
 modal()
+validation()
