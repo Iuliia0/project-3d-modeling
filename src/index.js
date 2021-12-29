826a9fa0b55919a9ee28aacf276910979ec65856
Iuliia Emelianova <julia03951@gmail.com>
2021-12-29 13:46:20 +0300

    Добавлено условие для модуля modal. Добавлено делегирование в модуле mume и modal. Выполнено первое задание урока 22

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
 import validation from './modules/validation'
+import tabs from './modules/tabs'
+
 
 
 timer('1 january 2022')
 menu()
 modal()
 validation()
+tabs()
