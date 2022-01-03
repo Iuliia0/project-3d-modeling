d2ea4ae06abe04c69ff82fc6fea0380ab85aad28
Iuliia Emelianova <julia03951@gmail.com>
2022-01-03 14:50:40 +0300

    Урок 24. Обязательное задание. Реализован калбклятор для рассчёта

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import modal from './modules/modal'
 import validation from './modules/validation'
 import tabs from './modules/tabs'
 import slider from './modules/slider'
+import calc from './modules/calc'
 
-timer('1 january 2022')
+
+timer('10 january 2022')
 menu()
 modal()
 validation()
 tabs()
 slider({containerSlider: '.portfolio-content', itemSlider: '.portfolio-item', containerDot: '.portfolio-dots'})
+calc(100)
