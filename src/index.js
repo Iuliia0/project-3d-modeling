c7b5760e07019344f28bf84727896305eedee06d
Iuliia Emelianova <julia03951@gmail.com>
2022-01-11 11:26:57 +0300

    Урок 27. Подключена отпрвка первой формы.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@ import validation from './modules/validation'
 import tabs from './modules/tabs'
 import slider from './modules/slider'
 import calc from './modules/calc'
+import sendForm from './modules/sendForm'
 
 
-timer('10 january 2022')
+timer('15 january 2022')
 menu()
 modal()
 validation()
 tabs()
 slider({containerSlider: '.portfolio-content', itemSlider: '.portfolio-item', containerDot: '.portfolio-dots'})
 calc(100)
+sendForm({ 
+  idForm: 'form1',
+  someElem: [
+    {
+      type: 'block',
+      id: 'total'
+    }
+  ]})
